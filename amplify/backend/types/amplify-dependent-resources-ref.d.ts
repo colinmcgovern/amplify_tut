98c6a720d7f40360045f2e40d368fb27542dac8d
colin mcgovern <colinmcg@udel.edu>
2023-04-26 09:44:00 -0400

    amplify working

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,26 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "api739093d6": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "CustomerHandler": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "hosting": {
+    "S3AndCloudFront": {
+      "HostingBucketName": "string",
+      "Region": "string",
+      "S3BucketSecureURL": "string",
+      "WebsiteURL": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Amplify, API } from 'aws-amplify'
+import React, { useEffect, useState } from 'react'
 
-function App() {
+const myAPI = "api739093d6"
+const path = '/customers'; 
+
+const App = () => {
+  const [input, setInput] = useState("")
+  const [customers, setCustomers] = useState([])
+
+  //Function to fetch from our backend and update customers array
+  function getCustomer(e) {
+    let customerId = e.input
+    API.get(myAPI, path + "/" + customerId)
+       .then(response => {
+         console.log(response)
+         let newCustomers = [...customers]
+         newCustomers.push(response)
+         setCustomers(newCustomers)
+
+       })
+       .catch(error => {
+         console.log(error)
+       })
+  }
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
+      <h1>Super Simple React App</h1>
+      <div>
+          <input placeholder="customer id" type="text" value={input} onChange={(e) => setInput(e.target.value)}/>      
+      </div>
+      <br/>
+      <button onClick={() => getCustomer({input})}>Get Customer From Backend</button>
+
+      <h2 style={{visibility: customers.length > 0 ? 'visible' : 'hidden' }}>Response</h2>
+      {
+       customers.map((thisCustomer, index) => {
+         return (
+        <div key={thisCustomer.customerId}>
+          <span><b>CustomerId:</b> {thisCustomer.customerId} - <b>CustomerName</b>: {thisCustomer.customerName}</span>
+        </div>)
+       })
+      }
     </div>
-  );
+  )
 }
 
 export default App;
