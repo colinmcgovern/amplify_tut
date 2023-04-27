02ae9b48d9a6abe0a453e6aa298c87eb744b7d70
colin mcgovern <colinmcg@udel.edu>
2023-04-27 14:01:24 -0400

    Adding comment forum

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import CommentAdd from '../CommentAdd';
+
+const App = () = {
+	<div>
+		<CommentAdd/>
+	</div>
+}
+
+export default App;
---
+++ src/components/AddComment.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import '../App.css';
+
+const AddComment = () => {
+
+	return (
+		<>
+			<table>
+				<tr>
+					<td>Username</td>
+					
+				</tr>
+			</table>
+		</>
+	)
+};
+
+export default AddComment;
